25f81f24b988d76b6a8af15d7bbeb327e159f4e6
Tiago Ilieve <tiago.myhro@gmail.com>
2015-11-29 23:56:13 -0200

    main: add 'check_host' task

---
+++ main/tasks.py
@@ -0,0 +1,15 @@
+import logging
+import subprocess
+from .models import Host
+from .models import Ping
+
+
+def check_host(host_id):
+    logging.basicConfig()
+    host = Host.objects.get(pk=host_id)
+    logging.info('Checking ' + host.hostname)
+    proc = subprocess.Popen(['ping', '-c 4', host.hostname], stdout=subprocess.PIPE)
+    return_code = proc.wait()
+    success = not bool(return_code)
+    Ping(host=host, success=success).save()
+    logging.info('Check for {host} saved.'.format(host=host.hostname))
