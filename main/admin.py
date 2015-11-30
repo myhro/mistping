c74fe9dc96807e7cd1c02595f15c25cf7656ebce
Tiago Ilieve <tiago.myhro@gmail.com>
2015-11-29 23:56:45 -0200

    main: add Models to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Host
+from .models import Ping
 
-# Register your models here.
+
+class PingAdmin(admin.ModelAdmin):
+    list_display = ('host', 'timestamp', 'success')
+
+
+admin.site.register(Host)
+admin.site.register(Ping, PingAdmin)
